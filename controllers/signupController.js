2967a74970de9cd158af4f47f3c04944b4130516
brandon-viterbo <brandonjviterbo@gmail.com>
2025-08-26 18:25:17 -0400

    Implement user sign-up

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -1,5 +1,26 @@
 const bcrypt = require("bcryptjs");
+const { PrismaClient } = require("../generate/prisma");
+
+const db = new PrismaClient();
 
 exports.getSignup = async (req, res) => {
   res.render("signup", { title: "Sign Up", user: false });
+};
+
+exports.addUser = async (req, res, next) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    await db.user.create({
+      data: {
+        email: req.body.email,
+        name: req.body.username,
+        password: hashedPassword,
+      },
+    });
+
+    res.redirect("/");
+  } catch (err) {
+    console.error(err);
+    return next(err);
+  }
 };
--- routes/signupRouter.js
+++ routes/signupRouter.js
@@ -1,8 +1,44 @@
 const { Router } = require("express");
 const { body, validationResult } = require("express-validator");
+const { PrismaClient } = require("../generate/prisma");
 const signupController = require("../controllers/signupController");
 const signupRouter = Router();
+const db = new PrismaClient();
 
 signupRouter.get("/", signupController.getSignup);
+signupRouter.post(
+  "/addUser",
+  body("email")
+    .notEmpty()
+    .trim()
+    .isEmail()
+    .custom(async (value) => {
+      const existingUser = await db.user.findUnique({
+        where: {
+          email: value,
+        },
+      });
 
+      if (existingUser) {
+        throw new Error("Email address already used");
+      }
+    })
+    .withMessage("Email address already used"),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be of length 6"),
+  body("confirmPassword")
+    .custom((value, { req }) => {
+      return value === req.body.password;
+    })
+    .withMessage("Passwords don't match"),
+  (req, res, next) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.send({ errors: result.array() });
+    }
+
+    signupController.addUser(req, res, next);
+  },
+);
 module.exports = signupRouter;
