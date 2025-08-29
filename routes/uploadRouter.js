5dee7110f944cb914928c8f3d00e3572d303b78f
brandon-viterbo <brandonjviterbo@gmail.com>
2025-08-28 23:33:20 -0400

    Setup multer for file upload (TODO later)

--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -1,10 +1,13 @@
 const { Router } = require("express");
 const uploadController = require("../controllers/uploadController");
+const multer = require("multer")
+const upload = multer({ dest: "uploads/" })
 const uploadRouter = new Router();
 
 uploadRouter.get("/", uploadController.getUploads);
-uploadRouter.post("/", (req, res) => {
+uploadRouter.post("/", upload.single("image"), (req, res) => {
   console.log(req.body.image);
   res.redirect("/");
 });
+
 module.exports = uploadRouter;
