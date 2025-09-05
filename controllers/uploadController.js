8d6ba8f3ef741a8a06eb78370478622f10fe2aae
brandon-viterbo <brandonjviterbo@gmail.com>
2025-09-05 18:46:20 -0400

    Enable uploads to server's local storage. TODO: db interactions

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,4 +1,5 @@
-exports.uplaodFile = async (req, res, next) => {
-  console.log(`Uploading image ${req.body.image} to folder ${req.params.folderId} owned by user ${req.user.id}`)
+exports.uploadFile = async (req, res, next) => {
+  //console.log(`Uploading image ${req.body.uploaded_file} to folder ${req.params.folderId} owned by user ${req.user.id}`)
+  //console.log(req.file)
   res.redirect("/");
 }
--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -1,10 +1,20 @@
 const { Router } = require("express");
 const uploadController = require("../controllers/uploadController");
-const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
 const uploadRouter = new Router();
 
-uploadRouter.post("/:folderId", upload.single("image"), uploadController.uplaodFile
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, `uploads/`)
+  },
+  filename: function (req, file, cb) {
+    const uniquePrefix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, uniquePrefix + '-' + file.originalname)
+  }
+})
+const upload = multer({ storage: storage });
+
+uploadRouter.post("/:folderId", upload.single("uploaded_file"), uploadController.uploadFile
 );
 
 module.exports = uploadRouter;
